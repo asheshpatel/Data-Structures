7bfaf64b35c16160d04d2f377c95b388e22827ad
Kelsey Helms <kelseyhelms@gmail.com>
2016-08-07 13:54:52 -0700

    Update Q1.c

--- CS261/Assignment1/Q1.c
+++ CS261/Assignment1/Q1.c
@@ -1,11 +1,13 @@
-/*  CS261- Assignment 1 - Q.1*/
-/*  Name: Kelsey Helms
- *  Date: June 27, 2016
- *  Solution description: Struct of students
- *  that generates unique ID and scores, then
- *  calculates minimum, maximum and average
- *  scores.
- */
+/***********************************************************
+ * Author:          Kelsey Helms
+ * Date Created:    July 27, 2016
+ * Filename:        Q1.c
+ *
+ * Overview:
+ * This program will generate unique ID and scores for a
+ * struct of students, then calculate the minimum,
+ * maximum, and average scores.
+ ************************************************************/
 
 #include <stdio.h>
 #include<stdlib.h>
@@ -17,27 +19,43 @@ struct student
     int score;
 };
 
+
+/***********************************************************
+ * allocate: allocates memory for 10 students, then
+ * returns a pointer to the struct.
+ *
+ * parameters: none.
+ * returns: pointer to struct student.
+ ***********************************************************/
+ 
 struct student* allocate()
 {
-    /*Allocate memory for ten students*/
-    struct student *ptr = malloc(10 * sizeof(struct student));
+    struct student *ptr = malloc(10 * sizeof(struct student));  // allocate memory for ten students
     
-    /*return the pointer*/
-    return ptr;
+    return ptr;                                                 // return the pointer
 }
 
+
+/***********************************************************
+ * generate: generates random and unique ID and scores
+ * for the ten students, ID between 1-10, scores 
+ * between 0-100.
+ *
+ * parameters: pointer to struct student.
+ * returns: none.
+ ***********************************************************/
+ 
 void generate(struct student* students)
 {
-    /*Generate random and unique ID and scores for ten students, ID being between 1 and 10, scores between 0 and 100*/
-    int used[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int used[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};                  // array of used numbers
     int id;
     _Bool flag;
-    for (int c = 0; c < 10; c++)
+    for (int c = 0; c < 10; c++)                                // fill all ten students
     {
-        do
-        {
+        do                                                      // continue trying to find unique ID while
+        {                                                       // number has already been used
             flag = 0;
-            id = rand() % 10 + 1;
+            id = rand() % 10 + 1;                               // make sure number is between 1-10
             for (int i = 0; i < 9; i++)
             {
                 if (id == used[i])
@@ -45,34 +63,44 @@ void generate(struct student* students)
             }
         } while (flag == 1);
         
-        used[c] = id;
-        students[c].id = id;
-        students[c].score = rand() % 101;
+        used[c] = id;                                           // put number into used array
+        students[c].id = id;                                    // set ID
+        students[c].score = rand() % 101;                       // randomly generate score between 0-100 and set score
     }
 }
 
+
+/***********************************************************
+ * output: displays struct student.
+ *
+ * parameters: pointer to struct student.
+ * returns: none.
+ ***********************************************************/
+ 
 void output(struct student* students)
 {
-    /*Output information about the ten students in the format:
-     ID1 Score1
-     ID2 score2
-     ID3 score3
-     ...
-     ID10 score10*/
-    for (int c = 0; c < 10; c++)
+    for (int c = 0; c < 10; c++)                                // output information about the ten students
     {
         printf("ID %i Score %i\n", students[c].id, students[c].score);
     }
 }
 
+
+/***********************************************************
+ * summary: computes and prints the minimum, maximum,
+ * and average scores of the ten students.
+ *
+ * parameters: pointer to struct student.
+ * returns: none.
+ ***********************************************************/
+ 
 void summary(struct student* students)
 {
-    /*Compute and print the minimum, maximum and average scores of the ten students*/
-    int min = 100;
+    int min = 100;                                              // set up initial min, max, and average
     int max = 0;
     double av = 0;
     
-    for (int c = 0; c < 10; c++)
+    for (int c = 0; c < 10; c++)                                // compare each student to min and max, then add to average
     {
         if (students[c].score < min)
             min = students[c].score;
@@ -81,17 +109,32 @@ void summary(struct student* students)
         av = av + students[c].score;
     }
     
-    av = av / 10;
-    
-    printf("Minimum: %i\nMaximum: %i\nAverage: %f", min, max, av);
+    av = av / 10;                                               // divide sum by number of students for average
+                                                                // display min, max, and average
+    printf("Minimum: %i\nMaximum: %i\nAverage: %f", min, max, av); 
 }
 
+
+/***********************************************************
+ * deallocate: deallocates memory from struct student.
+ *
+ * parameters: pointer to struct student.
+ * returns: none.
+ ***********************************************************/
+ 
 void deallocate(struct student* stud)
 {
-    /*Deallocate memory from stud*/
     free(stud);
 }
 
+
+/***********************************************************
+ * main: makes calls to all functions.
+ *
+ * parameters: none.
+ * returns: none.
+ ***********************************************************/
+ 
 int main()
 {
     struct student* stud = NULL;
