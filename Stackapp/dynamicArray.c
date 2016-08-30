f143c480aea38703eebc3f260ec02fcb5695fba1
Kelsey Helms <kelseyhelms@gmail.com>
2016-08-29 21:10:28 -0700

    Update dynamicArray.c

--- Stackapp/dynamicArray.c
+++ Stackapp/dynamicArray.c
@@ -4,7 +4,7 @@
  * Filename:        dynamicArray.c
  *
  * Overview:
- * This is the dynamic array implementation
+ * This is the dynamic array implementation file
  ************************************************************/
 
 #include <assert.h>
@@ -23,233 +23,238 @@ struct DynArr
 	Dynamic Array Functions
  ************************************************************************ */
 
-/* Initialize (including allocation of data array) dynamic array.
- 
-	param: 	v		pointer to the dynamic array
-	param:	cap 	capacity of the dynamic array
-	pre:	v is not null
-	post:	internal data array can hold cap elements
-	post:	v->data is not null
- */
+/***********************************************************
+ * initDynArr: initialize the array
+ *
+ * parameters: pointer to struct and capacity of array.
+ * returns: none.
+ ***********************************************************/
+
 void initDynArr(DynArr *v, int capacity)
 {
-    assert(capacity > 0);
+    assert(capacity > 0);    // assert capacity is not zero and the pointer to struct is not null
     assert(v!= 0);
-    v->data = (TYPE *) malloc(sizeof(TYPE) * capacity);
-    assert(v->data != 0);
-    v->size = 0;
+    
+    v->data = (TYPE *) malloc(sizeof(TYPE) * capacity);    // allocate memory for array 
+    
+    assert(v->data != 0);    // assert pointer to array is not null
+    
+    v->size = 0;    // initialize size to zero since empty and capacity to set capacity
     v->capacity = capacity;
 }
 
-/* Allocate and initialize dynamic array.
- 
-	param:	cap 	desired capacity for the dyn array
-	pre:	none
-	post:	none
-	ret:	a non-null pointer to a dynArr of cap capacity
- and 0 elements in it.
- */
+
+/***********************************************************
+ * newDynArr: allocate and initialize new array
+ *
+ * parameters: capacity of array.
+ * returns: pointer to DynArr struct.
+ ***********************************************************/
+
 DynArr* newDynArr(int cap)
 {
-    assert(cap > 0);
-    DynArr *r = (DynArr *)malloc(sizeof( DynArr));
-    assert(r != 0);
-    initDynArr(r,cap);
-    return r;
+    assert(cap > 0);    // assert capacity is not zero
+    
+    DynArr *r = (DynArr *)malloc(sizeof( DynArr));    // allocate memory for struct
+    
+    assert(r != 0);    // assert pointer to struct is not null
+    
+    initDynArr(r,cap);    // initialize array
+    
+    return r;    // return pointer to struct
 }
 
-/* Deallocate data array in dynamic array.
- 
-	param: 	v		pointer to the dynamic array
-	pre:	none
-	post:	d.data points to null
-	post:	size and capacity are 0
-	post:	the memory used by v->data is freed
- */
+
+/***********************************************************
+ * freeDynArr: deallocates array
+ *
+ * parameters: pointer to struct.
+ * returns: none.
+ ***********************************************************/
+
 void freeDynArr(DynArr *v)
 {
     if(v->data != 0)
     {
-        free(v->data); 	/* free the space on the heap */
-        v->data = NULL;   	/* make it point to null */
+        free(v->data);    // free array and make pointer null
+        v->data = NULL; 
     }
-    v->size = 0;
+    
+    v->size = 0;    // set size and capacity to zero
     v->capacity = 0;
 }
 
-/* Deallocate data array and the dynamic array ure.
- 
-	param: 	v		pointer to the dynamic array
-	pre:	none
-	post:	the memory used by v->data is freed
-	post:	the memory used by d is freed
- */
+
+/***********************************************************
+ * deleteDynArr: deallocate array and struct
+ *
+ * parameters: pointer to struct.
+ * returns: none.
+ ***********************************************************/
+
 void deleteDynArr(DynArr *v)
 {
-    freeDynArr(v);
-    free(v);
+    freeDynArr(v);    // deallocate array
+    free(v);    // free pointer to struct
 }
 
-/* Resizes the underlying array to be the size cap
- 
-	param: 	v		pointer to the dynamic array
-	param:	cap		the new desired capacity
-	pre:	v is not null
-	post:	v has capacity newCap
- */
+
+/***********************************************************
+ * _dynArrSetCapacity: resizes array to given capacity
+ *
+ * parameters: pointer to struct and capacity of array.
+ * returns: none.
+ ***********************************************************/
+
 void _dynArrSetCapacity(DynArr *v, int newCap)
 {
+    assert(newCap > 0);    // assert capacity is not zero and pointer to struct is not null
     assert(v!=0);
     
-    assert(newCap > 0);
-    
-    //    make a new array given the new capacity
-    struct DynArr newArray;
+    struct DynArr newArray;    // create a new array given the new capacity and initialize
     initDynArr(&newArray, newCap);
     
-    //    copy the old array to the new array
-    for (int index = 0; index < v->size; index++)
+    for (int index = 0; index < v->size; index++)    // copy the old array to the new array
     {
         newArray.data[index] = v->data[index];
         newArray.size++;
     }
     
-    //    release the old array
-    freeDynArr(v);
-    
-    //    point to the new array
-    *v = newArray;
+    freeDynArr(v);    // release the old array
+    *v = newArray;    // point to the new array
 }
 
-/* Get the size of the dynamic array
- 
-	param: 	v		pointer to the dynamic array
-	pre:	v is not null
-	post:	none
-	ret:	the size of the dynamic array
- */
+
+/***********************************************************
+ * sizeDynArr: get the size of the array
+ *
+ * parameters: pointer to struct.
+ * returns: size of array.
+ ***********************************************************/
+
 int sizeDynArr(DynArr *v)
 {
     return v->size;
 }
 
-/* 	Adds an element to the end of the dynamic array
- 
-	param: 	v		pointer to the dynamic array
-	param:	val		the value to add to the end of the dynamic array
-	pre:	the dynArry is not null
-	post:	size increases by 1
-	post:	if reached capacity, capacity is doubled
-	post:	val is in the last utilized position in the array
- */
+
+/***********************************************************
+ * addDynArr: adds an element to end of array
+ *
+ * parameters: pointer to struct and element value.
+ * returns: none.
+ ***********************************************************/
+
 void addDynArr(DynArr *v, TYPE val)
 {
-    /* Check to see if a resize is necessary */
-    if(v->size >= v->capacity)
+    if(v->size >= v->capacity)    // check to see if a resize is necessary
         _dynArrSetCapacity(v, 2 * v->capacity);
-    v->data[v->size] = val;
-    v->size++;
+        
+    v->data[v->size] = val;    // add element to end
+    v->size++;    // increase size of array
 }
 
-/*	Get an element from the dynamic array from a specified position
-	
-	param: 	v		pointer to the dynamic array
-	param:	pos		integer index to get the element from
-	pre:	v is not null
-	pre:	v is not empty
-	pre:	pos < size of the dyn array and >= 0
-	post:	no changes to the dyn Array
-	ret:	value stored at index pos
- */
+
+/***********************************************************
+ * getDynArr: get element from specified position of array
+ *
+ * parameters: pointer to struct.
+ * returns: element value.
+ ***********************************************************/
 
 TYPE getDynArr(DynArr *v, int pos)
 {
-    assert(v!=0);
+    assert(v!=0);    // assert pointer to struct is not null and position is not less than zero or greater than size of array
     assert(pos >= 0);
     assert(pos < v->size);
     
-    return v->data[pos];
+    return v->data[pos];    // return element value at specified position
 }
 
-/*	Put an item into the dynamic array at the specified location,
-	overwriting the element that was there
- 
-	param: 	v		pointer to the dynamic array
-	param:	pos		the index to put the value into
-	param:	val		the value to insert
-	pre:	v is not null
-	pre:	v is not empty
-	pre:	pos >= 0 and pos < size of the array
-	post:	index pos contains new value, val
- */
+
+/***********************************************************
+ * putDynArr: put an item into array at specified position,
+ *		overwrititng element that was there
+ *
+ * parameters: pointer to struct, specified position, and
+ *		element value.
+ * returns: none.
+ ***********************************************************/
+
 void putDynArr(DynArr *v, int pos, TYPE val)
 {
-    assert(v!=0);
+    assert(v!=0);    // assert pointer to struct is not null and position is not less than zero or greater than size of array
     assert(pos < v->size);
     assert(pos >= 0);
-    v->data[pos] = val;
+    
+    v->data[pos] = val;    // add element value at specified position
 }
 
-/*	Swap two specified elements in the dynamic array
- 
-	param: 	v		pointer to the dynamic array
-	param:	i,j		the elements to be swapped
-	pre:	v is not null
-	pre:	v is not empty
-	pre:	i, j >= 0 and i,j < size of the dynamic array
-	post:	index i now holds the value at j and index j now holds the value at i
- */
+
+/***********************************************************
+ * swapDynArr: swap two specified elements in array
+ *
+ * parameters: pointer to struct and two specified positions.
+ * returns: none.
+ ***********************************************************/
+
 void swapDynArr(DynArr *v, int i, int  j)
 {
-    assert (v!=0);
+    assert (v!=0);    // assert pointer to struct is not null and both positions are not less than zero or greater than size of array
     assert(i >=0 && i < v->size);
     assert(j >=0 && j < v->size);
-    int temp = v->data[i];
+    
+    int temp = v->data[i];    // swap elements using a temp to help
     v->data[i] = v->data[j];
     v->data[j] = temp;
 }
 
-/*	Remove the element at the specified location from the array,
-	shifts other elements back one to fill the gap
- 
-	param: 	v		pointer to the dynamic array
-	param:	idx		location of element to remove
-	pre:	v is not null
-	pre:	v is not empty
-	pre:	idx < size and idx >= 0
-	post:	the element at idx is removed
-	post:	the elements past idx are moved back one
- */
+
+/***********************************************************
+ * removeatDynArr: removes element at specified position 
+ * 		and shifts other elements back to fill gap
+ *
+ * parameters: pointer to struct and specified position.
+ * returns: none.
+ ***********************************************************/
+
 void removeAtDynArr(DynArr *v, int idx)
 {
-    assert(v!=0);
+    assert(v!=0);    // assert pointer to struct is not null and position is not less than zero or greater than size of array
     assert(v->size > idx);
     assert(idx >= 0);
     
-    for (int j = idx; j < v->capacity; j++)
+    for (int j = idx; j < v->capacity; j++)    // shift all elements that are greater than specified position back one
         v->data[j] = v->data[j+1];
-    v->size = v->size - 1;
+        
+    v->size = v->size - 1;    // decrease size of array
 }
-
 
 
 /* ************************************************************************
 	Stack Interface Functions
  ************************************************************************ */
 
-/*	Returns boolean (encoded in an int) demonstrating whether or not the
-	dynamic array stack has an item on it.
- 
-	param:	v		pointer to the dynamic array
-	pre:	the dynArr is not null
-	post:	none
-	ret:	1 if empty, otherwise 0
- */
+/***********************************************************
+ * isEmptyDynArr: returns boolean as int (1 if empty, 0 if not) 
+ * 		to show if array contains any element
+ *
+ * parameters: pointer to struct.
+ * returns: boolean as int.
+ ***********************************************************/
+
 int isEmptyDynArr(DynArr *v)
 {
     return (sizeDynArr(v) == 0);
 }
 
+
+/***********************************************************
+ * _dynArrSetCapacity: resizes array to given capacity
+ *
+ * parameters: pointer to struct and capacity of array.
+ * returns: none.
+ ***********************************************************/
 /* 	Push an element onto the top of the stack
  
 	param:	v		pointer to the dynamic array
